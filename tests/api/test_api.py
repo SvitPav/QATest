d163db0858cfdf4724827a4896f162d855b6d60c
SvitPav <svetlight68@gmail.com>
2024-05-22 18:41:18 +0200

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,4 +1,16 @@
+class User:
 
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Sergii"
+        self.second_name = "Butenko"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
 
 
 def test_remove_name(user):
