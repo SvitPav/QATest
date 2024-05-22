38b7ea5c2c8bab3151bae61f615a44bb32f489ab
SvitPav <svetlight68@gmail.com>
2024-05-22 18:25:41 +0200

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -2,13 +2,13 @@
 
 
 def test_remove_name(user):
-    user.name = ''
-    assert user.name == ''
+    user.name = ""
+    assert user.name == ""
 
 def test_name(user):
-    assert user.name == 'Sergii'
+    assert user.name == "Sergii"
 
 def test_second_name(user):
-    assert user.second_name =='Butenko'
+    assert user.second_name =="Butenko"
 
 
