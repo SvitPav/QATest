8bf92ca64b1b1f4aa4a3281f01dd1400be472aae
SvitPav <svetlight68@gmail.com>
2024-05-22 18:45:29 +0200

    Update test_fixture.py

--- tests/api/test_fixture.py
+++ tests/api/test_fixture.py
@@ -1,6 +1,17 @@
-def test_change_name(user):
-    assert user.name == "Sergii"
+import pytest
+from user import User
 
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
+
+def test_change_name(user):
+    assert user.name == 'Sergii'
 
 def test_change_second_name(user):
-    assert user.second_name == "Butenko"
+    assert user.second_name == 'Butenko'
