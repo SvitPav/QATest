1225408c03a12cc2722d71729d652043af7c4c7b
SvitPav <svetlight68@gmail.com>
2024-05-22 18:33:57 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,21 +1,5 @@
 import pytest
 
-
-class User:
-
-    def __init__(self) -> None:
-        self.name = None
-        self.second_name = None
-
-    def create(self):
-        self.name = "Sergii"
-        self.second_name = "Butenko"
-
-    def remove(self):
-        self.name = ""
-        self.second_name = ""
-
-
 @pytest.fixture
 def user():
     user = User()
