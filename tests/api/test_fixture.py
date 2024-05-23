17c1eca5448a3e4a4ac50835cb939c05ddac0075
SvitPav <svetlight68@gmail.com>
2024-05-23 06:19:35 +0200

    Update test_fixture.py

--- tests/api/test_fixture.py
+++ tests/api/test_fixture.py
@@ -1,15 +1,11 @@
 import pytest
-from user import User  # Зміна цього рядка на абсолютний шлях
 
-@pytest.fixture
-def user():
-    user = User()
-    user.create()
-    yield user
-    user.remove()
 
+@pytest.mark.check
 def test_change_name(user):
     assert user.name == 'Sergii'
 
+
+@pytest.mark.check
 def test_change_second_name(user):
     assert user.second_name == 'Butenko'
