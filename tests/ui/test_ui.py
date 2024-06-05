7f0c95c0a859de22e610f8d868c16f71bdfea943
SvitPav <svetlight68@gmail.com>
2024-06-05 02:09:09 +0200

    Update test_ui.py

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,14 +1,12 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 @pytest.mark.ui
 def test_check_incorrect_username():
-    # Створення об'єкту для керування бразуером
+    # Створення об'єкту для керування браузером
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     # відкриваємо сторінку https://github.com/login 
@@ -17,7 +15,7 @@ def test_check_incorrect_username():
     # Знаходимо поле, в яке будемо вводити неправильне ім'я користувача або поштову адресу
     login_elem = driver.find_element(By.ID, "login_field")
 
-    # Вводимо неправильне ім'я користувача або поштову адрІесу
+    # Вводимо неправильне ім'я користувача або поштову адресу
     login_elem.send_keys("sergiibutenko@mistakeinemail.com")
 
     # Знаходимо поле, в яке будемо вводити неправильний пароль
