d3442a0a65211788c59d5f6625f09063bed94648
SvitPav <svetlight68@gmail.com>
2024-06-06 18:32:48 +0200

    Update base_page.py

--- modules/ui/page_objects/base_page.py
+++ modules/ui/page_objects/base_page.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 class BasePage:
     def __init__(self) -> None:
         self.driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().instal()))
+            service=Service(ChromeDriverManager().install()))
             
 
     def close(self):
