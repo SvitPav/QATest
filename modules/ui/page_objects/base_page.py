d43e7e4e96e9f323bc32867ba7670941cd8dae47
SvitPav <svetlight68@gmail.com>
2024-06-06 18:26:05 +0200

    Update base_page.py

--- modules/ui/page_objects/base_page.py
+++ modules/ui/page_objects/base_page.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrom import ChromDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class BasePage:
