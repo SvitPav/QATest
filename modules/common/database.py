04eb996464dceca22668b69f094a74b69971e4e4
SvitPav <svetlight68@gmail.com>
2024-06-08 14:58:59 +0200

    Update database.py

--- modules/common/database.py
+++ modules/common/database.py
@@ -1,71 +1,15 @@
-import pytest
-from modules.common.database import Database
+import sqlite3
 
 
-@pytest.mark.database
-def test_database_connection():
-    db = Database()
-    db.test_connection()
+class Database():
 
+    def __init__(self):
+        self.connection = sqlite3.connect(r'/home/sbutenko/repos/LnD/Become QA Auto' + r'/become_qa_auto.db')
+        self.cursor = self.connection.cursor()
 
-@pytest.mark.database
-def test_check_all_users():
-    db = Database()
-    users = db.get_all_users()
-
-    print(users)
-
-
-@pytest.mark.database
-def test_check_user_sergii():
-    db = Database()
-    user = db.get_user_address_by_name('Sergii')
-
-    assert user[0][0] == 'Maydan Nezalezhnosti 1'
-    assert user[0][1] == 'Kyiv'
-    assert user[0][2] == '3127'
-    assert user[0][3] == 'Ukraine'
-
-
-@pytest.mark.database
-def test_product_qnt_update():
-    db = Database()
-    db.update_product_qnt_by_id(1, 25)
-    water_qnt = db.select_product_qnt_by_id(1)
-
-    assert water_qnt[0][0] == 25
-
-
-@pytest.mark.database
-def test_product_insert():
-    db = Database()
-    db.insert_product(4, 'печиво', 'солодке', 30)
-    water_qnt = db.select_product_qnt_by_id(4)
-
-    assert water_qnt[0][0] == 30
-
-
-@pytest.mark.database
-def test_product_delete():
-    db = Database()
-    db.insert_product(99, 'тестові', 'дані', 999)
-    db.delete_product_by_id(99)
-    qnt = db.select_product_qnt_by_id(99)
-
-    assert len(qnt) == 0
-
-
-@pytest.mark.database
-def test_detailed_orders():
-    db = Database()
-    orders = db.get_detailed_orders()
-    print("Замовлення", orders)
-    # Check quantity of orders equal to 1
-    assert len(orders) == 1
-
-    # Check struture of data
-    assert orders[0][0] == 1
-    assert orders[0][1] == 'Sergii'
-    assert orders[0][2] == 'солодка вода'
-    assert orders[0][3] == 'з цукром'
+    def test_connection(self):
+        sqlite_select_Query = "SELECT sqlite_version();"
+        self.cursor.execute(sqlite_select_Query)
+        record = self.cursor.fetchall()
+        print(f"Connected successfully. SQLite Database Version is: {record}")
 
